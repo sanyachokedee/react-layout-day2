bbf3992c3103ed6855562cebd2eaad6204a549f7
Nutthakit Anansuksakul <ing118199@gmail.com>
2022-01-31 08:51:40 +0700

    footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -58,9 +58,9 @@ function Footer() {
           <div className="d-flex justify-content-between py-4 my-4 border-top">
             <p>© 2021 Company, Inc. All rights reserved.</p>
             <ul className="list-unstyled d-flex">
-              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#twitter" /></svg></a></li>
-              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#instagram" /></svg></a></li>
-              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#facebook" /></svg></a></li>
+              <li className="ms-3"><a className="link-dark" href="www.google.cp.th"><svg className="bi" width={24} height={24}><use xlinkHref="#twitter" /></svg></a></li>
+              <li className="ms-3"><a className="link-dark" href="www.google.cp.th"><svg className="bi" width={24} height={24}><use xlinkHref="#instagram" /></svg></a></li>
+              <li className="ms-3"><a className="link-dark" href="www.google.cp.th"><svg className="bi" width={24} height={24}><use xlinkHref="#facebook" /></svg></a></li>
             </ul>
           </div>
         </footer>
